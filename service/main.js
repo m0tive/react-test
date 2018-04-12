ac5d4a80606ebf87b44a7c09ceb7d3f185d420d6
Peter Dodds <peter@pddds.com>
2018-04-12 21:49:05 +0100

    Add rover endpoint, list rovers react

--- service/main.js
+++ service/main.js
@@ -1,11 +1,32 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http, { path: '/api', serveClient: false });
-
-app.get('/*', (_req, res) => res.status(404).end());
+const io = require('socket.io')(http, { path: '/ws', serveClient: false });
 
 io.on('connection', socket => {
     console.log('connection!');
 });
+
+const rovers = [
+    {name: "spirit"},
+    {name: "opportunity"},
+]
+
+// Allow cross origin requests
+app.get('*', (_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+        .header('Access-Control-Allow-Methods', 'GET')
+        .header('Access-Control-Allow-Headers', 'Content-Type');
+    next()
+});
+
+app.get('/rovers', (_req, res) => res.json(rovers).end());
+
+setTimeout(function emit_rovers() {
+    io.emit('/rovers', rovers);
+}, 1000);
+
+// all other traffic
+app.get('/*', (_req, res) => res.status(404).end());
+
 
 http.listen(4444, () => console.log('listening on *:4444'));
--- src/App.js
+++ src/App.js
@@ -5,25 +5,37 @@ import './App.css';
 import io from 'socket.io-client'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // eslint-disable-next-line no-restricted-globals
+    const apiHost = `${location.protocol}//${location.hostname}:4444`;
+
+    const socket = io(apiHost, { path: '/ws' });
+
+    socket.on('connect', () => console.log('cool'));
+
+    this.state = {
+      rovers: []
+    };
+
+    fetch(apiHost + '/rovers')
+      .then(response => response.json())
+      .then(rovers => {
+        this.setState({rovers: rovers});
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          {this.state.rovers.map(rover => <li>{rover.name}</li> )}
+        </ul>
       </div>
     );
   }
 }
-
-// eslint-disable-next-line no-restricted-globals
-const socket = io(`${location.protocol}//${location.hostname}:4444`, { path: '/api' });
-
-socket.on('connect', () => console.log('cool'));
 
 
 export default App;
